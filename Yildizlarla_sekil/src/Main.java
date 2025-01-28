7a0fbc02f1d08fc50fee99a6ca3db26a544bc57b
hmgny <hmerveguney@hotmail.com>
2025-01-28 08:53:59 +0300

    Yıldızlarla üçgen oluşturuldu

---
+++ Yildizlarla_sekil/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz.");
+        n = input.nextInt();
+
+        for(int i=1; i<=n; i++){
+            System.out.println(" ".repeat(n-i)+"*".repeat(i+(i-1))+" ".repeat(n-i));
+        }
+    }
+}
